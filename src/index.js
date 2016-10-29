703b6e0f37f06df8a617af357a28a9be2994e700
JMonono <julian.monono@gmail.com>
2016-10-29 20:10:37 +0100

    +Added React Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import { hashHistory } from 'react-router';
-import './index.css';
+// import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
   <Routes history={hashHistory} />,

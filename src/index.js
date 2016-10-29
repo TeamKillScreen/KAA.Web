8721fb8b5416b97ffb9272f3f37565619d4ba0bd
JMonono <julian.monono@gmail.com>
2016-10-29 19:54:52 +0100

    + Trying hashHistory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import './index.css';
 
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <Routes history={hashHistory} />,
   document.getElementById('root')
 );

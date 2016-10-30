348c4b1318cf3d81db60b3a0942fa346ec8c7bdf
JMonono <julian.monono@gmail.com>
2016-10-30 09:18:49 +0000

    + Added links to people on home page

--- src/components/home.js
+++ src/components/home.js
@@ -19,7 +19,32 @@ class Home extends Component {
                         <span className="spacer" />
                         <Link className="btn btn-info btn-lg" role="button"
                               to="/portal/3F3A393B3B36373A333A">
-                            My missing person
+                            My missing person - Craig
+                        </Link>
+                        <span className="spacer" />
+                        <Link className="btn btn-info btn-lg" role="button"
+                              to="/portal/393A3A3B3B36373A333A">
+                            My missing person - Gemma
+                        </Link>
+                        <span className="spacer" />
+                        <Link className="btn btn-info btn-lg" role="button"
+                              to="/portal/3A3A3A3B3B36373A333A">
+                            My missing person - Jo
+                        </Link>
+                        <span className="spacer" />
+                        <Link className="btn btn-info btn-lg" role="button"
+                              to="/portal/383A3A3B3B36373A333A">
+                            My missing person - Julian
+                        </Link>
+                        <span className="spacer" />
+                        <Link className="btn btn-info btn-lg" role="button"
+                              to="/portal/403A393B3B36373A333A">
+                            My missing person - Richard
+                        </Link>
+                        <span className="spacer" />
+                        <Link className="btn btn-info btn-lg" role="button"
+                              to="/portal/413A393B3B36373A333A">
+                            My missing person - Yak
                         </Link>
                     </p>
                 </Grid>
